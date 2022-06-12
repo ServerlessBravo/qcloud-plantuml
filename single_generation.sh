abb17d9dd99ab437c33eda922a361986bbd8c822
Zhengquan Yang <yangzhengquan@gmail.com>
2022-06-12 23:11:15 +0800

    Update single_generation.sh

--- single_generation.sh
+++ single_generation.sh
@@ -1,11 +1,11 @@
 #/usr/bin/env bash
 
-echo 'cleaning files in qcloudicons'
+echo 'cleaning files in tmp_icons'
 
 rm -rf ./tmp_icons/*.png
 rm -rf ./tmp_icons/*.puml
 
-echo 'begin to copy shared pumls'
+echo 'begin to generate umls under tmp_icons'
 
 
 ./create_sprites.sh -p ./tmp_icons -w 48 -h 48 -s Qcloud
